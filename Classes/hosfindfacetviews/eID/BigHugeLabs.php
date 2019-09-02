bfd2139fcbff004e0e88b204aa0e760d855e97dd
Schleevoigt <rainer.schleevoigt@sub.uni-hamburg.de>
2019-09-02 15:31:21 +0200

    Wording OpenAccess

--- Classes/hosfindfacetviews/eID/BigHugeLabs.php
+++
@@ -1,5 +0,0 @@
-<?php
-$needle = \TYPO3\CMS\Core\Utility\GeneralUtility::_GP('needle');
-$url = 'http://words.bighugelabs.com/api/2/4831e32b4d2b828ca1ad0c070355c0b1/' . urlencode($needle) . '/json';
-header('Content-type: application/json');
-echo  file_get_contents($url);
