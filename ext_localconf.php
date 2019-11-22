888ce719f9100e57ce8f030ce0d4e5efc09c54ac
Schleevoigt <rainer.schleevoigt@sub.uni-hamburg.de>
2019-11-22 11:13:20 +0100

    eID moved to Classes/Utility/Ajax

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,13 +2,13 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 //$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['tile'] = 'EXT:'.$_EXTKEY . '/eID/GetTile.php';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['sitemap'] = 'EXT:'. $_EXTKEY.'/eID/Sitemap.php';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['orcid'] = 'EXT:'.$_EXTKEY.'/eID/Orcid.php';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['altmetrics'] = 'EXT:'.$_EXTKEY.'/eID/AltMetrics.php';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['detail'] = 'EXT:'. $_EXTKEY.'/eID/Detail.php';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['wms'] = 'EXT:'. $_EXTKEY.'/eID/GetWMS.php';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['thumbnail'] = 'EXT:'. $_EXTKEY.'/eID/Thumbnail.php';
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['og'] = 'EXT:'. $_EXTKEY.'/eID/GetOpenGraph.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['sitemap'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/Sitemap.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['orcid'] = 'EXT:'.$_EXTKEY.'/Classes/Utility/Ajax/Orcid.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['altmetrics'] = 'EXT:'.$_EXTKEY.'/Classes/Utility/Ajax/AltMetrics.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['detail'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/Detail.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['wms'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/GetWMS.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['thumbnail'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/Thumbnail.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['og'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/GetOpenGraph.php';
 
 $TYPO3_CONF_VARS['EXTCONF']['realurl'] =  array(
         '_DEFAULT' => array(
