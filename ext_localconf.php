682fdecd39a4c0558663107ad7b3c78104529b78
haogatyp <gatermann@effective-webwork.de>
2020-11-18 12:13:23 +0100

    Remove unused ajax eid scripts.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,9 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-//$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['tile'] = 'EXT:'.$_EXTKEY . '/eID/GetTile.php';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['sitemap'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/Sitemap.php';
-// $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['orcid'] = 'EXT:'.$_EXTKEY.'/Classes/Utility/Ajax/Orcid.php';
-// $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['altmetrics'] = 'EXT:'.$_EXTKEY.'/Classes/Utility/Ajax/AltMetrics.php';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['detail'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/Detail.php';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['wms'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/GetWMS.php';
-// $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['thumbnail'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/Thumbnail.php';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['solr'] = 'EXT:'. $_EXTKEY.'/Classes/Utility/Ajax/GetSolr.php';
 
 $TYPO3_CONF_VARS['EXTCONF']['realurl'] =  array(
