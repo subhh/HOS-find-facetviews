0aca5e3d3584c58ff0c97251a02ce92e7dacd8f1
David Maus <david.maus@sub.uni-hamburg.de>
2020-11-24 14:16:49 +0100

    Proxy Geodienste neu geschrieben

--- Classes/Utility/Ajax/GetWMS.php
+++ Classes/Utility/Ajax/GetWMS.php
@@ -1,59 +1,41 @@
 <?php
-/*
- *  (c) 2019 Rainer Schleevoigt <rainer.schleevoigt@sub.uni-hamburg.de>
- *      Hamburg State and University Library
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- * ************************************************************* */
+
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-const TTL = 24*3600*100;
-$CACHEFOLDER = 'typo3temp/wmszwischenspeicher';
-$endpoint = "https://geodienste.hamburg.de/HH_WMS_Geobasiskarten_GB";//GeneralUtility::_GP('endpoint');
+$requestBaseUrl = "https://geodienste.hamburg.de/HH_WMS_Geobasiskarten_GB";
 
-$layers =  GeneralUtility::_GP('layers');
-$srs =  GeneralUtility::_GP('srs');
-$size =  GeneralUtility::_GP('retina')*256;
-$bbox =  GeneralUtility::_GP('bbox');
-$key = 'wms_'. $bbox;
+$requestParams = array(
+    'service' => 'WMS',
+    'request' => 'GetMap',
+    'styles' => '',
+    'version' => '1.3.0',
+    'transparent' => 'true',
+    'format' => 'image/png',
+    'layers' => GeneralUtility::_GP('layers'),
+    'width'  => GeneralUtility::_GP('retina') * 256,
+    'height' => GeneralUtility::_GP('retina') * 256,
+    'crs'    => GeneralUtility::_GP('srs'),
+    'bbox'   => GeneralUtility::_GP('bbox'),
+);
+$requestUrl = $requestBaseUrl . '?' . http_build_query($requestParams);
 
-$querystring= 'service=WMS&request=GetMap&styles=&version=1.3.0&transparent=true&format=image/png';
-$querystring .= '&bbox='.$bbox.'&crs='.$srs.'&layers='.$layers .'&width='.$size .'&height='.$size;
-$url= $endpoint . '?' . $querystring;
+$cacheTtl = 24 * 3600; // 24h Cache
+$cacheKey = base64_encode($requestUrl);
 
-$key = base64_encode($url);
-
-$response = apc_fetch($key);
-if ($response === false) {
+$response = apc_fetch($cacheKey);
+if (!$response) {
     $context = stream_context_create(["http" => ["ignore_errors" => true]]);
-    $response = file_get_contents($url, false, $context);
+    $response = file_get_contents($requestUrl, false, $context);
 
     preg_match('{HTTP\/\S*\s(\d{3})}', $http_response_header[0], $match);
     $status = (int) $match[1];
-
     if ($status >= 400 || !$response) {
         error_log("Geodienst nicht erreichbar Code: " . $status . " ( " . $url . " )", 0);
         http_response_code(502);
         exit();
     }
 
-    apc_add($key, $response, TTL);
+    apc_add($cacheKey, $response, $cacheTtl);
 }
 
 header('Content-type: image/png');
